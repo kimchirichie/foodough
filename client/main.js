997b8694a02a8c62d4dea9f2705dead16b4403df
brainfogz <sinr0202@gmail.com>
2017-12-14 17:37:04 -0500

    changed to allow cordova to bootstrap

--- client/main.js
+++ client/main.js
@@ -1,6 +1,19 @@
 import angular from "angular";
 import angularMeteor from "angular-meteor";
 import uiRouter from 'angular-ui-router';
-
+import { Meteor } from 'meteor/meteor';
 import { name as Foodough } from "../imports/ui/components/foodough/foodough";
 
+function onReady() {
+  angular.bootstrap(document, [
+    Foodough
+  ], {
+    strictDi: true
+  });
+}
+ 
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
