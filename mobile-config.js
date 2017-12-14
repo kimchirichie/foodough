96b50da8d0e296d28d65c410758dc2856a7e5e9c
brainfogz <sinr0202@gmail.com>
2017-12-14 15:31:24 -0500

    removed splash

--- mobile-config.js
+++ mobile-config.js
@@ -8,41 +8,6 @@ App.info({
   website: 'https://money.kimchirichie.com'
 });
 
-App.launchScreens({
-
-  // iOS splash screens
-  iphone5: 'resources/launch_screens/iphone5.png',
-  iphone6: 'resources/launch_screens/iphone6.png',
-  iphone6p_portrait: 'resources/launch_screens/iphone6p_portrait.png',
-  iphone6p_landscape: 'resources/launch_screens/iphone6p_landscape.png',
-  iphoneX_portrait: 'resources/launch_screens/iphoneX_portrait.png',
-  iphoneX_landscape: 'resources/launch_screens/iphoneX_landscape.png',
-  ipad_portrait_2x: 'resources/launch_screens/ipad_portrait_2x.png',
-  ipad_landscape_2x: 'resources/launch_screens/ipad_landscape_2x.png',
-  ipad_portrait_pro_10_5: 'resources/launch_screens/ipad_portrait_pro_10_5.png',
-  ipad_landscape_pro_10_5: 'resources/launch_screens/ipad_landscape_pro_10_5.png',
-  ipad_portrait_pro_12_9: 'resources/launch_screens/ipad_portrait_pro_12_9.png',
-  ipad_landscape_pro_12_9: 'resources/launch_screens/ipad_landscape_pro_12_9.png',
-
-  // iOS splash screens legacy
-  iphone_2x: 'resources/launch_screens/iphone_2x.png',
-  ipad_portrait: 'resources/launch_screens/ipad_portrait.png',
-  ipad_landscape: 'resources/launch_screens/ipad_landscape.png',
-
-  // Android splash screens
-  android_mdpi_portrait: 'resources/launch_screens/android_mdpi_portrait.png',
-  android_mdpi_landscape: 'resources/launch_screens/android_mdpi_landscape.png',
-  android_hdpi_portrait: 'resources/launch_screens/android_hdpi_portrait.png',
-  android_hdpi_landscape: 'resources/launch_screens/android_hdpi_landscape.png',
-  android_xhdpi_portrait: 'resources/launch_screens/android_xhdpi_portrait.png',
-  android_xhdpi_landscape: 'resources/launch_screens/android_xhdpi_landscape.png',
-  android_xxhdpi_portrait: 'resources/launch_screens/android_xxhdpi_portrait.png',
-  android_xxhdpi_landscape: 'resources/launch_screens/android_xxhdpi_landscape.png',
-  android_xxxhdpi_portrait: 'resources/launch_screens/android_xxxhdpi_portrait.png',
-  android_xxxhdpi_landscape: 'resources/launch_screens/android_xxxhdpi_landscape.png'
-
-});
-
 App.icons({
   iphone_2x: 'resources/icons/iphone_2x.png',
   iphone_3x: 'resources/icons/iphone_3x.png',
