8aa26ca79838ab024050c34c8bbc0d1b3ab1e1b1
brainfogz <sinr0202@gmail.com>
2017-12-14 14:53:19 -0500

    added mobilbile mobile icon and splash screen

---
+++ mobile-config.js
@@ -0,0 +1,62 @@
+// This section sets up some basic app metadata, the entire section is optional.
+App.info({
+  id: 'com.kimchirichie.money',
+  name: 'foodough',
+  description: 'Track your spending with foodough',
+  author: 'Richard Sin',
+  email: 'richie@kimchirichie.com',
+  website: 'https://money.kimchirichie.com'
+});
+
+App.launchScreens({
+
+  // iOS splash screens
+  iphone5: 'resources/launch_screens/iphone5.png',
+  iphone6: 'resources/launch_screens/iphone6.png',
+  iphone6p_portrait: 'resources/launch_screens/iphone6p_portrait.png',
+  iphone6p_landscape: 'resources/launch_screens/iphone6p_landscape.png',
+  iphoneX_portrait: 'resources/launch_screens/iphoneX_portrait.png',
+  iphoneX_landscape: 'resources/launch_screens/iphoneX_landscape.png',
+  ipad_portrait_2x: 'resources/launch_screens/ipad_portrait_2x.png',
+  ipad_landscape_2x: 'resources/launch_screens/ipad_landscape_2x.png',
+  ipad_portrait_pro_10_5: 'resources/launch_screens/ipad_portrait_pro_10_5.png',
+  ipad_landscape_pro_10_5: 'resources/launch_screens/ipad_landscape_pro_10_5.png',
+  ipad_portrait_pro_12_9: 'resources/launch_screens/ipad_portrait_pro_12_9.png',
+  ipad_landscape_pro_12_9: 'resources/launch_screens/ipad_landscape_pro_12_9.png',
+
+  // iOS splash screens legacy
+  iphone_2x: 'resources/launch_screens/iphone_2x.png',
+  ipad_portrait: 'resources/launch_screens/ipad_portrait.png',
+  ipad_landscape: 'resources/launch_screens/ipad_landscape.png',
+
+  // Android splash screens
+  android_mdpi_portrait: 'resources/launch_screens/android_mdpi_portrait.png',
+  android_mdpi_landscape: 'resources/launch_screens/android_mdpi_landscape.png',
+  android_hdpi_portrait: 'resources/launch_screens/android_hdpi_portrait.png',
+  android_hdpi_landscape: 'resources/launch_screens/android_hdpi_landscape.png',
+  android_xhdpi_portrait: 'resources/launch_screens/android_xhdpi_portrait.png',
+  android_xhdpi_landscape: 'resources/launch_screens/android_xhdpi_landscape.png',
+  android_xxhdpi_portrait: 'resources/launch_screens/android_xxhdpi_portrait.png',
+  android_xxhdpi_landscape: 'resources/launch_screens/android_xxhdpi_landscape.png',
+  android_xxxhdpi_portrait: 'resources/launch_screens/android_xxxhdpi_portrait.png',
+  android_xxxhdpi_landscape: 'resources/launch_screens/android_xxxhdpi_landscape.png'
+
+});
+
+App.icons({
+  iphone_2x: 'resources/icons/iphone_2x.png',
+  iphone_3x: 'resources/icons/iphone_3x.png',
+  ipad: 'resources/icons/ipad.png',
+  ipad_2x: 'resources/icons/ipad_2x.png',
+  ipad_pro: 'resources/icons/ipad_pro.png',
+  ios_settings: 'resources/icons/ios_settings.png',
+  ios_settings_2x: 'resources/icons/ios_settings_2x.png',
+  ios_settings_3x: 'resources/icons/ios_settings_3x.png',
+  ios_spotlight: 'resources/icons/ios_spotlight.png',
+  ios_spotlight_2x: 'resources/icons/ios_spotlight_2x.png',
+  android_mdpi: 'resources/icons/android_mdpi.png',
+  android_hdpi: 'resources/icons/android_hdpi.png',
+  android_xhdpi: 'resources/icons/android_xhdpi.png',
+  android_xxhdpi: 'resources/icons/android_xxhdpi.png',
+  android_xxxhdpi: 'resources/icons/android_xxxhdpi.png',
+});
